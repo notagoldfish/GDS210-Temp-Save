fc0080962e8c62fcc598b73470283d15245363bf
notagoldfish <61007801+notagoldfish@users.noreply.github.com>
2020-11-26 20:00:05 +0800

    Temp Save from Thursday 26/11/2020
    Yeah yeah yeah

---
+++ Assets/Slicing Test/CutCollision.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CutCollision : MonoBehaviour
+{
+    //private void OnTriggerEnter(Collision collision)
+    //{
+    //    CubeCut.Cut(collision.transform, transform.position);
+    //}
+
+    //private void OnTriggerEnter(Collider other)
+    //{
+    //    CubeCut.Cut(other.transform, transform.position);
+    //}
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.CompareTag("Woodchops"))
+        {
+            CubeCut.Cut(other.transform, transform.position);
+        }
+    }
+}
